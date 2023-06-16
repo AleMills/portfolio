fb45126e6e284b66e9638f57c0ea405ab1074c04
Alejandro Jofre <alejandrojofre949@gmail.com>
2023-06-16 17:55:10 -0300

    reparacion

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -3,19 +3,17 @@ import "../stylesheet/Inputs.css"
 import { useState } from "react";
 
 export function Inputs() {
-    const [inputValue, setInputValue] = useState();
     const [enviado, setEnviado] = useState(false)
     const texto = enviado ? "Enviado" : "Enviar"
     const enviar = () => {
-        setInputValue("")
         setEnviado(true)
     }
 
     return (
         <div className="columna">
-            <input type="text" value={inputValue} name="name" id="name" placeholder="Tu nombre"/>
-            <input type="email" value={inputValue} name="email" id="email" placeholder="Tu correo"/>
-            <textarea value={inputValue} name="message" id="message" cols="30" rows="10" placeholder="Tu mensaje"></textarea>
+            <input type="text" name="name" id="name" placeholder="Tu nombre"/>
+            <input type="email" name="email" id="email" placeholder="Tu correo"/>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Tu mensaje"></textarea>
             <button onClick={enviar} type="submit" className={`btn-formulario ${texto}`}>{texto}</button>
         </div>
     )
