6fa8217e0aa77029f6209779f013acf292949cca
alemills <alejandrojofre949@gmail.com>
2024-09-03 16:38:53 -0300

    change colors

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -1,11 +1,14 @@
 import "../stylesheet/Proyecto.css"
 
 export function Proyecto({ imagen, titulo, descripcion, vinculo }) {
+
+    const imageSrc = new URL(`../img/proyecto${imagen}.jpg`, import.meta.url).href;
+
     return (
             <div className="proyecto">
                 <div className="overlay"></div>
                 <a href={vinculo}>
-                    <img src={`../src/img/proyecto${imagen}.jpg`} alt="proyecto"/>
+                    <img src={imageSrc} alt="proyecto"/>
                 </a>
                 <div className="info">
                     <h4>{titulo}</h4>
